add453a34a491bd6afb93f78b5ef72071c4388af
Petar Gavran <pg003521048@fer.hr>
2022-07-24 16:12:49 +0200

    first commit

---
+++ src/test/java/hr/kingakademija/zadatak/mini_web_shop/MiniWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.kingakademija.zadatak.mini_web_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
